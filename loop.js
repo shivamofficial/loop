cdecb6956516bb3ac7dcd40500196c443d887f02
shivamofficial <91952992+shivamofficial@users.noreply.github.com>
2022-03-08 14:51:18 +0530

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,47 @@
+let i;
+for(i=0;i<10;i++)
+    {
+console.log(i);
+    }
+
+    for(let j=1; j<10;j++)
+    {
+       
+        console.log("19*" + j +"="+ 19*j);
+    }
+ let k=0
+for( ; k<10; k++)
+{
+    console.log(hello);
+}
+let j=0;
+for( ; j<10; )
+{
+    console.log ("bye");
+    j++;
+}
+
+//while loop  while(condition){.......}
+let r=0;
+while (i<10)
+{
+console.log("value of r is " ,r);
+r++;
+}
+console.log("outside loop");
+
+let m=0;
+while (m<10)
+{
+console.log("value of m is " ,m);
+m++;
+}
+console.log("outside loop");
+let t=0;
+do
+{
+    console.log("value of t is", t);
+    t++;
+}
+while(t<10)
+
